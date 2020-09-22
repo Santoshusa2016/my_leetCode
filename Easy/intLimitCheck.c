c3864c96be5eec9009d099d2319cd56f3964776c
santoshusa2016 <santoshusa2016@gmail.com>
2020-09-22 00:40:22 -0500

    first commit

---
+++ Easy/intLimitCheck.c
@@ -0,0 +1,30 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+  
+  int addOvf(int* result, int a, int b) 
+ { 
+     *result = a + b; 
+    printf("result Val %d\n", result);
+    printf("result Address %p\n", result);
+    printf("result Val * %d\n", *result);
+
+
+     if(a > 0 && b > 0 && *result < 0) 
+         return -1; 
+     if(a < 0 && b < 0 && *result > 0) 
+         return -1; 
+     return 0; 
+ } 
+  
+ int main() 
+ { 
+     int *res = (int *)malloc(sizeof(int)); 
+     int x = 21474836; 
+     int y = 10; 
+  
+     printf("%d", addOvf(res, x, y)); 
+  
+     printf("\n %d", *res); 
+     getchar(); 
+     return 0; 
+} 
---
+++ Easy/reverseint.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int lenOfArray(int data){
+    int m = data;
+    int digit = 0;
+    while (m) {
+        digit++; 
+        // Truncate the last digit from the number 
+        m /= 10; 
+    }
+    return digit;
+}
+
+long reverse(int data){
+    int arrayLen = lenOfArray(data);
+    int *arr = (int *)malloc(4 * arrayLen);
+    long retlongVal = 0;
+
+    for (int i = 0; i <= arrayLen-1; i++)
+    {
+        int value = data%10;       
+        *(arr + i ) = value;
+        data = data/=10;
+    }
+   
+    for (int i = 0; i < arrayLen; i++)
+    { 
+      retlongVal = 10 * retlongVal + *(arr + i);          
+    }
+
+    printf("%ld \n", retlongVal);
+    if(retlongVal > INT_MAX || retlongVal < INT_MIN){
+        
+        return 0;
+    }
+    return retlongVal;
+}
+
+void main(){
+    int num; 
+    long revnum;
+    printf("type a number \n");
+    int converted = scanf("%d", &num);
+    //Check if input is valid number
+    if( converted == 0) 
+    {
+        printf("Invalid Input");
+    }
+    else{
+        revnum = reverse(num);
+        printf("Reverse of number is:%d", revnum);
+    }
+}
+

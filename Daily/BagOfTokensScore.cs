8c02858b2cc590ab0581098d0335175343a298cb
Santoshusa2016 <santosh3737@gmail.com>
2022-09-12 19:12:31 -0500

    DailyChallenge 09/12/2022

---
+++ Daily/BagOfTokensScore.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetCode.Daily
+{
+    /*
+     * Ref: https://leetcode.com/problems/bag-of-tokens/
+     * LeetCode: 948 Bag of Tokens 
+     * Date: 09/12/2022
+     * Test case:[100],50; [100,200],150; [100,200,300,400],200
+     * Failed:
+     * Hint: sort the array and take 2 pointer, begin pointer- using power buy stocks, end pointer using vote buy max stock
+     * Time Complexity:
+     * Space Complexity:
+     */
+    internal class BagOfTokensScore
+    {
+        int Solve(int[] tokens, int power)
+        {
+            int maxVotes, votes;
+            maxVotes = votes = 0;
+
+            //step01: sort array asc
+            tokens = tokens.OrderBy(a => a).ToArray();
+
+            //step 01: take 2 pointer & start iteration
+            int begin = 0;
+            int end = tokens.Length - 1;
+
+            while (begin <= end)
+            {
+                /*
+                 * we cannot use power/votes becaus both can increase/decrease
+                 * != begin<length, end>0: we sell our max vote share to buy other, so we cannot buy it again since all
+                 * money is used. If we need to buy again we need to use Vote, but problem is to get max vote - Adani-NDTV
+                 */
+
+                if (tokens[begin] <= power)
+                {
+                    //step 3: if tokens[i] < power ->buy
+                    votes += 1;
+                    power -= tokens[begin];
+                    begin++;
+                }
+                else if (votes > 0)
+                {
+                    //step 4: if tokens[i]> power -> use score to buy
+                    votes -= 1;
+                    power += tokens[end];
+                    end--;
+                }
+                else { 
+                    //step 05: default case when you dont have enough vote/score & money/power to buy
+                    break;
+                }
+
+                //maxVote: since in each iteration votes can vary and we need count of max.
+                maxVotes = Math.Max(maxVotes, votes);
+            }
+
+            return maxVotes;
+        }
+
+        public void Driver()
+        {
+            int maxVote = Solve(new int[] { 100, 200, 300, 400 }, 200);
+            Console.WriteLine("maxVote: {0}", maxVote);
+        }
+    }
+}

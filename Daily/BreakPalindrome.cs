b77cf4e77e3a7b46f142eb9f43d3ee7a0b272cc7
Santoshusa2016 <santosh3737@gmail.com>
2022-10-11 02:34:02 -0500

    1328. Break a Palindrome - 10/10/2022

---
+++ Daily/BreakPalindrome.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetCode.Daily
+{
+    /*
+    * Ref: https://leetcode.com/problems/break-a-palindrome/
+    * LeetCode: 1328. Break a Palindrome
+    * hint: greedy alg
+    * Date: 10/10/2022
+    * Test case: aa, abccba
+    * Time Complexity: 
+    * Space Complexity:
+    */
+
+    internal class BreakPalindrome
+    {
+
+        string solve(string palindrome)
+        {
+            char charA = 'a';
+            int len = palindrome.Length;
+
+            //base case when str len = 1
+            if (len <= 1)
+                return "";
+
+            char[] palindromes = palindrome.ToCharArray();
+            
+            //for palindrome to negate we just need to change any 1 char till midpoint
+            for (int i = 0; i < len/2; i++)
+            {
+                //if any char in list is not A, replace it with B
+                if (palindromes[i] != charA)
+                {
+                    palindromes[i] = charA;
+                    return string.Concat(palindromes);
+                }
+            }
+
+            //when all chars are a, replace the last one with b.
+            palindromes[len-1] = 'b';
+            return string.Concat(palindromes);
+
+        }
+        public void Driver()
+        {
+            var retval = solve("aa");
+            Console.WriteLine("BreakPalindrome:{0}", retval);
+        }
+    }
+}

cf1a11daf1ef432570d8a13f71dab582766da276
Santosh Iyengar <santosh3737@gmail.com>
2022-10-13 01:21:06 -0500

    976. Largest Perimeter Triangle - 10/12/2022

---
+++ Arrays/GroupAnagrams.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetCode.Arrays
+{
+    internal class GroupAnagrams
+    {
+        IList<IList<string>> solve(string[] strs)
+        {
+            IList<IList<string>> retVal = new List<IList<string>>();
+            if (strs.Length == 0)
+            {
+                return retVal;
+            }
+
+            Dictionary<string, List<string>> anagrams = 
+                new Dictionary<string, List<string>>();
+
+            foreach (var str in strs)
+            {
+                string key = string.Concat(str.OrderBy(a => a));
+                if (anagrams.ContainsKey(key))
+                {
+                    anagrams[key].Add(str);
+                }
+                else
+                {
+                    anagrams.Add(key, new List<string>(new string[] { str }));
+                }
+            }
+
+            foreach (var item in anagrams)
+            {
+                retVal.Add(item.Value);
+            } 
+            return retVal;
+        }
+        
+        public void Driver()
+        {
+            var inputVal = new string[]
+            {
+                //"eat", "tea", "tan", "ate", "nat", "bat"
+                "aa"
+            };
+            solve(inputVal);
+
+            Console.WriteLine("GroupAnagrams");
+        }
+    }
+}
---
+++ Daily/LargestPerimeter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetCode.Daily
+{
+    /*
+     * Ref: https://leetcode.com/problems/largest-perimeter-triangle/submissions/
+     * LeetCode: 976. Largest Perimeter Triangle
+     * Date: 10/12/2022
+     * Hashtag: #perimeter, #sort
+     * Test case:[2 1 2], [1 2 1]
+     * Failed:[3,6,2,3]
+     * Hint: 0/1 Knapsack
+     * Time Complexity:
+     * Space Complexity:
+     */
+    internal class LargestPerimeter
+    {
+        int solve(int[] nums)
+        {
+            int perimeter = 0;
+            if (nums.Length < 3)
+                return perimeter;
+
+            //step01: sort the array asc.
+            Array.Sort(nums);
+
+            //since array is sorted asc, the last value can be taken as c(base)
+            for (int i = nums.Length - 1; i >=2; i--)
+            {
+                int c = nums[i];
+                int b = nums[i - 1];
+                int a = nums[i - 2];
+
+                //Scalene triangle = a+b>c
+                if (a + b > c)
+                { 
+                    perimeter = (a + b + c);
+                    break;
+                }
+            }
+            return perimeter;
+        }
+
+        public void Driver()
+        {
+            var inputVar = new int[] { 3, 6, 2, 3 };
+            var retVal = solve(inputVar);
+            Console.WriteLine("LargestPerimeter:{0}", retVal);
+
+        }
+    }
+}

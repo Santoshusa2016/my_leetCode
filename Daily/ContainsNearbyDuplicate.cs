c630721677339086e8783211addeeb714d4bf674
Santosh Iyengar <santosh3737@gmail.com>
2022-10-23 20:39:08 -0500

    219. Contains Duplicate II - 10/21/2022

---
+++ Daily/ContainsNearbyDuplicate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace my_leetCode.Daily
+{
+    internal class ContainsNearbyDuplicate
+    {
+        bool solve(int[] nums, int k)
+        {
+            Dictionary<int, List<int>> kv = new Dictionary<int, List<int>>();
+
+            //find 2 index having same val
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!kv.ContainsKey(nums[i]))
+                {
+                    kv.Add(nums[i], new List<int>(new int[] { i }));
+                }
+                else
+                {
+                    var existVal = kv[nums[i]].Last();
+                    if (Math.Abs(i - existVal) <= k)
+                        return true;
+                    kv[nums[i]].Add(i);
+                }                
+            }
+            return false;
+        }
+        public void Driver()
+        {
+            int[] nums = new int[] { 1, 2, 3, 1 };
+            int k = 3;
+            solve(nums, k);
+            Console.WriteLine("ContainsNearbyDuplicate");
+        }
+    }
+}

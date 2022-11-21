c551bc6756fcffc2de156d8f95ac2b00b3bc4314
Santosh Iyengar <santosh3737@gmail.com>
2022-11-21 00:49:28 -0600

    224. Basic Calculator: 11/20/2022

---
+++ Daily/Calculate.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace my_leetCode.Daily
+{
+    /*
+    * Ref: https://leetcode.com/problems/basic-calculator/
+    * LeetCode:224. Basic Calculator
+    * Tag: #hard, #stack
+    * Company:
+    * Date: 11/20/2022
+    * Time Complexity: 
+    * Space Complexity:
+    */
+    internal class Calculate
+    {
+        int solve(string s)
+        {
+            s = s.Trim();
+            int index, leftOperand, sign, rightOperand, runningSum;
+            index = leftOperand = rightOperand = runningSum = 0;
+            sign = 1;
+            Stack<int> calcStack = new Stack<int>();
+
+            while (index < s.Length)
+            {
+                if (s[index] == '+' || s[index] == '-')
+                {
+                    leftOperand = runningSum;
+                    rightOperand = 0;
+                    sign = s[index] == '+' ? 1 : -1;
+                }
+                else if (s[index] == '(')
+                {
+                    if (sign != 0)
+                    {
+                        calcStack.Push(sign);
+                        calcStack.Push(runningSum);
+
+                        //reset the values
+                        leftOperand = rightOperand = 0;
+                        sign = 1;
+                    }
+                }
+                else if (s[index] == ')')
+                {
+                    //taking sum of values inside braces as right operand
+                    rightOperand = runningSum;
+                    if (calcStack.Count > 0)
+                    {
+                        leftOperand = calcStack.Pop();
+                        sign = calcStack.Pop();
+                    }
+                }
+                else if (s[index] != ' ')
+                {
+                    //base case
+                    rightOperand = rightOperand * 10 + (int)(s[index] - '0');
+                }
+
+                //calc running sum
+                runningSum = leftOperand + (sign * rightOperand);
+                index++;
+            }
+            return runningSum;
+        }
+
+        public void Driver()
+        {
+            var inputVal = "(1+(4+5+2)-3)+(6+8)";
+            var retVal = solve(inputVal);
+            Console.WriteLine($"Calculate-{retVal}");
+        }
+    }
+}
---
+++ Daily/RemoveDuplicates.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace my_leetCode.Daily
+{
+    /*
+    * Ref: https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+    * LeetCode:1047. Remove All Adjacent Duplicates In String
+    * Tag: #easy
+    * Company:
+    * Date: 11/10/2022
+    * Time Complexity: 
+    * Space Complexity:
+    */
+    internal class RemoveDuplicates
+    {
+        public string solve(string s)
+        {
+            int i = 1;
+            Stack<char> stackChars = new Stack<char>();
+            stackChars.Push(s[0]);
+
+            while (i < s.Length)
+            {
+                char prev = ' ';
+                if (stackChars.Count > 0)
+                    prev = stackChars.Peek();
+
+                if (prev != s[i])
+                    stackChars.Push(s[i]);
+                else
+                    stackChars.Pop();
+                i++;
+            }
+            return string.Concat(stackChars.Reverse());
+        }
+
+        public void Driver()
+        {
+            string inputReq = "azxxzy";
+            string retVal = solve(inputReq);
+            Console.WriteLine("RemoveDuplicates:" + retVal);
+        }
+    }
+}
